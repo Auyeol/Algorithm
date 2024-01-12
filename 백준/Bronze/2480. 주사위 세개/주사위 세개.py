1117ffd0709c0831c4fe37d0b5b04e2101ce20b4
Auyeol <154718012+Auyeol@users.noreply.github.com>
2024-01-12 14:52:14 +0900

    [Bronze IV] Title: 주사위 세개, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,11 @@
+a, b, c = map(int, input().split())
+money = 0
+if(a==b and b==c):
+  money = 10000 + a * 1000
+elif((a==b and b!=c) or (a==c and a!=b)):
+  money = 1000 + a * 100
+elif((b==c and a!=b)):
+  money = 1000 + b * 100 
+else:
+  money = max(a,b,c) * 100
+print(money)
