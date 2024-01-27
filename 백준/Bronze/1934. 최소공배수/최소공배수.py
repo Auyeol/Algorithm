eb756a61a6146f86e2ef80ec0febce945ef98e6c
Auyeol <154718012+Auyeol@users.noreply.github.com>
2024-01-27 18:03:05 +0900

    [Bronze I] Title: 최소공배수, Time: 128 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.py
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -1,16 +1,11 @@
-case_num = int(input())
-for case in range(case_num):
-    num1, num2 = list(map(int, input().split())) # 270 192
-    aList = [num1, num2]
-    while True:
-        temp = num1%num2 
-        num1 = num2 
-        num2 = temp
-        if temp == 0:
-            gcd = num1
-            break
+case_num = int(input()) 
 
-    result = 1
-    for num in aList:
-        result *= (num//gcd)
-    print(result*gcd)
+for case in range(case_num):
+    num1, num2 = list(map(int, input().split())) 
+    a, b = num1, num2
+    while b:
+        a, b = b, a%b
+    gcd = a
+
+    lcm = (num1 * num2) // gcd
+    print(lcm)
