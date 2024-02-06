a8bd0a95a48c66c4c2ad419c55a2799c5c2328a1
Auyeol <154718012+Auyeol@users.noreply.github.com>
2024-02-06 23:37:24 +0900

    [Bronze V] Title: 학점계산, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -0,0 +1,36 @@
+S = input()
+
+if S[0] == 'A':
+    if S[1] == '+':
+        print(4.3)
+    elif S[1] == '0':
+        print(4.0)
+    else:
+        print(3.7)
+
+if S[0] == 'B':
+    if S[1] == '+':
+        print(3.3)
+    elif S[1] == '0':
+        print(3.0)
+    else:
+        print(2.7)
+
+if S[0] == 'C':
+    if S[1] == '+':
+        print(2.3)
+    elif S[1] == '0':
+        print(2.0)
+    else:
+        print(1.7)
+
+if S[0] == 'D':
+    if S[1] == '+':
+        print(1.3)
+    elif S[1] == '0':
+        print(1.0)
+    else:
+        print(0.7)
+
+if S[0] == 'F':
+    print(0.0)
