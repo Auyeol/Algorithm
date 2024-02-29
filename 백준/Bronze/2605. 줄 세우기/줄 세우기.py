99c7f0d6ca86f62873e5f863a4bb06e3856a084d
Auyeol <154718012+Auyeol@users.noreply.github.com>
2024-02-29 12:03:43 +0900

    [Bronze II] Title: 줄 세우기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2605. 줄 세우기/줄 세우기.py
+++ 백준/Bronze/2605. 줄 세우기/줄 세우기.py
@@ -3,13 +3,11 @@ import sys
 N = int(sys.stdin.readline())
 arr = [(i+1) for i in range(N)]
 card_arr = list(map(int, sys.stdin.readline().split()))
-cur_idx = 0
+
 for i in range(len(card_arr)):
-    jump = card_arr[i] # 3이 들어오면 0,
-    cur_idx = i # 0, 1, 2, 3, 4 ....
-    for j in range(jump): # jump만큼 자리교체
+    jump = card_arr[i] 
+    for j in range(jump): 
         if i-j-1>=0:
             arr[i-j], arr[i-j-1] = arr[i-j-1], arr[i-j]
-
 
 print(*arr)
