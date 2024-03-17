37830416edbd3eb4aad25cb971177d54af2bb6db
Auyeol <154718012+Auyeol@users.noreply.github.com>
2024-03-17 18:08:42 +0900

    [Bronze II] Title: 분해합, Time: 1132 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -3,10 +3,8 @@ num = int(sys.stdin.readline())
 result = 0
 
 for i in range(1, num):
-    sum_v = i
-    arr = list(str(i))
-    for v in arr:
-        sum_v += int(v)
+    sum_v = i + sum(map(int, str(i)))
+    
     if sum_v == num:
         result = i
         break
