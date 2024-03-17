e60dda003d2ccc01dc4749a1b74ddf332734f95c
Auyeol <154718012+Auyeol@users.noreply.github.com>
2024-03-17 16:00:52 +0900

    [Bronze I] Title: 단어 공부, Time: 204 ms, Memory: 33076 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,22 +1,19 @@
 import sys
-
-# 입력 받기
-input_string = sys.stdin.readline().strip().upper()
-
-# 각 문자의 개수를 저장할 딕셔너리 생성
-char_count = {}
-for char in input_string:
-    if char in char_count:
-        char_count[char] += 1
+word = sys.stdin.readline().strip().upper()
+my_dict = {}
+for c in word:
+    if c in my_dict:
+        my_dict[c] += 1
     else:
-        char_count[char] = 1
+        my_dict[c] = 1
 
-# 가장 많이 등장한 문자와 그 등장 횟수 찾기
-max_count = max(char_count.values())
-max_chars = [char for char, count in char_count.items() if count == max_count]
+max_v = max(my_dict.values())
+arr = []
+for item in my_dict.items():
+    if item[1] == max_v:
+        arr.append(item[0])
 
-# 최댓값이 유일한지 확인
-if len(max_chars) == 1:
-    print(max_chars[0])
+if len(arr) >= 2:
+    print('?')
 else:
-    print("?")
+    print(arr[0])
