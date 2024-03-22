b37f6d5d110efe818177d497689dffb2506589b0
Auyeol <154718012+Auyeol@users.noreply.github.com>
2024-03-22 17:15:02 +0900

    [Bronze II] Title: 이진수 변환, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10829. 이진수 변환/이진수 변환.py
+++ 백준/Bronze/10829. 이진수 변환/이진수 변환.py
@@ -1,3 +1,14 @@
+# N = int(input())
+# result = bin(N)
+# print(result[2:])
+
 N = int(input())
-result = bin(N)
-print(result[2:])
+result = ''
+while N > 0:
+    if N % 2 == 1:
+        result = '1' + result
+    else:
+        result = '0' + result
+    N //= 2
+
+print(result)
