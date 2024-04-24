aaba97826e83b9bc8c51bf484a4e3dc8acefc581
Auyeol <154718012+Auyeol@users.noreply.github.com>
2024-04-24 23:31:53 +0900

    [Silver II] Title: DFS와 BFS, Time: 76 ms, Memory: 34052 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,52 +1,35 @@
-# 그래프를 DFS로 탐색한 결과, BFS로 탐색한 결과를 출력하는 프로그램 작성
-# 방문할 수 있는 점이 여러 개인 경우에는 정점 번호가 작은 것을 먼저 탐색
-# 방문할 수 있는 점이 없는 경우 종료
-# 정점 번호는 1~N
+import sys
 from collections import deque
+input = sys.stdin.readline
 
+def dfs(now):
+    dfs_result.append(now)
+    visited[now] = True
 
-def dfs(num):
-    visited[num] = True
-    path_dfs.append(num)
-
-    """
-    print('sorted -> ', sorted(G[num]))
-    sorted ->  [1, 4]
-    sorted ->  [2, 3]
-    sorted ->  [1, 5]
-    sorted ->  [2, 4]
-    sorted ->  [3, 5]
-    """
-
-    for w in sorted(G[num]):
+    for w in sorted(G[now]):
         if not visited[w]:
             dfs(w)
+    return dfs_result
 
-
-def bfs(num):
+def bfs(now):
     Q = deque()
-    visited = [False] * (N + 1)
-    Q.append(num)
-    visited[num] = True
-
+    visited = [False] * (N+1)
+    Q.append(now)
+    visited[now] = True
     while Q:
         v = Q.popleft()
-        path_bfs.append(v)
-
+        bfs_result.append(v)
         for w in sorted(G[v]):
             if not visited[w]:
-                visited[w] = True
                 Q.append(w)
-
-
-
+                visited[w] = True
 
 
 N, M, V = map(int, input().split())
 G = [[] for _ in range(N+1)]
 visited = [False] * (N+1)
-path_dfs = []
-path_bfs = []
+dfs_result = []
+bfs_result = []
 for _ in range(M):
     v1, v2 = map(int, input().split())
     G[v1].append(v2)
@@ -55,5 +38,5 @@ for _ in range(M):
 dfs(V)
 bfs(V)
 
-print(*path_dfs)
-print(*path_bfs)
+print(*dfs_result)
+print(*bfs_result)
