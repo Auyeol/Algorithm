bf3c857e5ef5bc937a2732c0ae544ca1e5ba2b75
Auyeol <154718012+Auyeol@users.noreply.github.com>
2024-04-30 10:46:33 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 636 ms, Memory: 76188 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -0,0 +1,31 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+def dfs(now):
+    global cnt
+    visited[now] = cnt
+
+    for w in sorted(G[now]):
+        if visited[w] == 0:
+            cnt += 1
+            dfs(w)
+
+    return visited
+
+cnt = 1
+
+N, M, R = map(int, input().split())
+G = [[] for _ in range(N+1)]
+result = []
+visited = [0] * (N + 1)
+for _ in range(M):
+    v1, v2 = map(int, input().split())
+
+    G[v1].append(v2)
+    G[v2].append(v1)
+
+result = dfs(R)
+
+for i in range(1, len(result)):
+    print(result[i])
