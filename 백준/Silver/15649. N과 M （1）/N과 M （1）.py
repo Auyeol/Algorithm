e5d713e9f3b5e6e91ef4a2f6b9a573f2d38f5805
Auyeol <154718012+Auyeol@users.noreply.github.com>
2024-05-27 14:46:51 +0900

    [Silver III] Title: N과 M (1), Time: 152 ms, Memory: 36580 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,21 +1,8 @@
-# 1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
-
-def dfs(now, lst):
-    if now == M:
-        S.append(lst)
-        return
-
-    for i in range(1, N+1):
-        if not visited[i]:
-            visited[i] = 1
-            dfs(now+1, lst + [i])
-            visited[i] = 0
+from itertools import permutations
 
 N, M = map(int, input().split())
-visited = [0] * (N+1)
-S = []
+numbers = [(i+1) for i in range(N)]
+result = list(permutations(numbers, M))
 
-dfs(0, [])
-
-for elem in S:
-    print(*elem)
+for i in result:
+    print(*i)
