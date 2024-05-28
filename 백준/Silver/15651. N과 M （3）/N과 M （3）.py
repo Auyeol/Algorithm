2883f6bf3ab8b292f053cdf56b4f5daff469ee43
Auyeol <154718012+Auyeol@users.noreply.github.com>
2024-05-28 10:20:14 +0900

    [Silver III] Title: N과 M (3), Time: 1972 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -1,18 +1,11 @@
-def dfs(now, lst):
-    if now == M:
-        S.append(lst)
+def dfs(now, result):
+    if now == M + 1:
+        print(*result)
         return
 
     for i in range(1, N+1):
-        if not visited[now]:
-            visited[now] = 1
-            dfs(now + 1, lst + [i])
-            visited[now] = 0
+        dfs(now+1, result + [i])
+
 
 N, M = map(int, input().split())
-visited = [0] * (N+1)
-S = []
-dfs(0,[])
-
-for elem in S:
-    print(*elem)
+dfs(1, [])
