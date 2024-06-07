b89a8b8e14335d786151fc2df491c75ce8c5d543
Auyeol <154718012+Auyeol@users.noreply.github.com>
2024-06-07 14:30:22 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -0,0 +1,8 @@
+def solution(numbers):
+    result = []
+    
+    for i in range(len(numbers)-1):
+        for j in range(i+1, len(numbers)):
+            result.append(numbers[i] * numbers[j])
+            
+    return max(result)
