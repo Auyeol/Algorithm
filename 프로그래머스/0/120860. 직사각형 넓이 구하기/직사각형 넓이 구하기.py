30e712ee7ffbfe580dd1eebc4bb5c476762fa417
Auyeol <154718012+Auyeol@users.noreply.github.com>
2024-06-18 15:25:43 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.py
@@ -0,0 +1,12 @@
+def solution(dots):
+    x = []
+    y = []
+    
+    for dot in dots:
+        x.append(dot[0])
+        y.append(dot[1])
+    
+    width = max(x) - min(x)
+    height = max(y) - min(y)
+    
+    return width * height
